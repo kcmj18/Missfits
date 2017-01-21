acb1f381fb8d8ad548bddb286909b205862b8699
Joe Rinehart <rinehartjm09@gmail.com>
2017-01-20 19:07:01 -0800

    Added page controller with some basic actions/views

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- root 'page#account'
+ root 'page#home'
 end
