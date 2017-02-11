3de9df322f6f62d381b9c65c5d6d0dc43cece154
Kacie Jones <kcmj1318@gmail.com>
2017-02-11 15:29:23 -0800

    added admin user alwasy default. Set true in db manually

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,7 @@
 
 class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
-
+  before_filter :authenticate_user!
   # GET /courses
   # GET /courses.json
   def index
