ad16069d563b446beddcff3070201041aaeebba3
Joe Rinehart <rinehartjm09@gmail.com>
2017-02-13 17:12:37 -0800

    Removed user input field, Auto generated user_id on form for adding class

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -69,6 +69,7 @@ class SchedulesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def schedule_params
+      # params.require(:schedule).permit(:course_id, :user_id)
       params.require(:schedule).permit(:course_id, :user_id)
     end
 end
